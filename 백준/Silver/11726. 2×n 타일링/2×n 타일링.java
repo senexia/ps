cbfb1ac0cd9f1bc3c25654fb366024ff792b84ea
senexia <128709556+senexia@users.noreply.github.com>
2025-07-28 21:12:19 +0900

    [Silver III] Title: 2×n 타일링, Time: 116 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3 ; i < 1001 ; i++) {
+            dp[i] = dp[i-1] % 10007 + dp[i-2] % 10007;
+        }
+        System.out.println(dp[n] % 10007);
+
+    }
+}
