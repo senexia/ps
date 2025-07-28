0e1a82dba1bb41dea6580f7191ef015b850dfee0
senexia <128709556+senexia@users.noreply.github.com>
2025-07-28 21:23:39 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws  Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String cat = "|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|";
+        bw.write(cat);
+        bw.flush();
+        bw.close();
+    }
+}
