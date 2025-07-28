2852581efc6c2aa87fcc71e0ab5ff378f131b8c7
senexia <128709556+senexia@users.noreply.github.com>
2025-07-28 21:23:19 +0900

    [Bronze V] Title: N 찍기, Time: 260 ms, Memory: 25572 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws  Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        for(int i = 1 ; i <= n ; i++) bw.write(String.valueOf(i) + "\n");
+
+
+        bw.flush();
+        bw.close();
+    }
+}
