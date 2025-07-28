3ea7a1a8dfd7203562462242ae205cbe30ad3292
senexia <128709556+senexia@users.noreply.github.com>
2025-07-28 21:24:59 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    static long f[];
+    public static long dp(int n) {
+        if(f[n] != 0) return f[n];
+        else {
+            f[n] = dp(n-1) + dp(n-2);
+            return f[n];
+        }
+    }
+    public static void main(String[] args) throws  Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        f = new long[91];
+        f[1] = 1;
+        f[2] = 1;
+        if(n <= 2) bw.write(String.valueOf(1));
+        else bw.write(String.valueOf(dp(n)));
+        bw.flush();
+        bw.close();
+
+    }
+}
