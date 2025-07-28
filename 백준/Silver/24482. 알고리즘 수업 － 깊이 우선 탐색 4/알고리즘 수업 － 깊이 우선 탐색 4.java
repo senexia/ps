c1acc6bce62af07f95e3ee09589ff4bc79aa80ec
senexia <128709556+senexia@users.noreply.github.com>
2025-07-28 21:10:09 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 1444 ms, Memory: 99316 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static List<List<Integer>> li = new ArrayList<>();
+    static int[] depth;
+    static boolean[] visited;
+
+    static void dfs(int x) {
+        visited[x] = true;
+        for(int num : li.get(x)) {
+            if(!visited[num]) {
+                if(depth[num] == -1) depth[num] = depth[x] + 1;
+                dfs(num);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        depth = new int[n + 1];
+        visited = new boolean[n + 1];
+        for(int i = 0; i <= n ; i++) li.add(new ArrayList<>());
+
+        for(int i = 0 ; i < m ; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int u1 = Integer.parseInt(st.nextToken());
+            int u2 = Integer.parseInt(st.nextToken());
+
+            li.get(u1).add(u2);
+            li.get(u2).add(u1);
+        }
+        for(List<Integer> l : li) Collections.sort(l, Comparator.reverseOrder());
+        Arrays.fill(depth, -1);
+        depth[v] = 0;
+        dfs(v);
+
+        for(int j = 1 ; j <= n ; j++) {
+            System.out.println(depth[j]);
+        }
+    }
+}
