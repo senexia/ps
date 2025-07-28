dd51d3085612ce6e7359f6332fb84a7eb06e6a87
senexia <128709556+senexia@users.noreply.github.com>
2025-07-28 21:31:39 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 1 ; i <= n ; i++) {
+            String countingstars = "*";
+            bw.write(countingstars.repeat(i) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
