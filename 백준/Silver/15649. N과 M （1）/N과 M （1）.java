3c74611896b701fe7adf29915863d4274f4d4ee8
senexia <128709556+senexia@users.noreply.github.com>
2025-07-29 00:48:31 +0900

    [Silver III] Title: N과 M (1), Time: 716 ms, Memory: 44492 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, r;
+    static int[] a, b;
+    static boolean[] v;
+
+    static void perm(int cnt) {
+        if(cnt == r) {
+            StringBuilder sb = new StringBuilder();
+            for(int i : b) {
+                sb.append(i).append(" ");
+            }
+            sb.setLength(sb.length() - 1);
+            System.out.println(sb);
+            return;
+        }
+        for(int i = 0 ; i < n ; i++) {
+            if(v[i]) continue;
+            v[i] = true;
+            b[cnt] = a[i];
+            perm(cnt + 1);
+            v[i] = false;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        a = new int[n];
+        b = new int[r];
+        v = new boolean[n];
+        for(int i = 0 ; i < n ; i++) {
+            a[i] = i + 1;
+        }
+
+        perm(0);
+    }
+}
