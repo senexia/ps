5f1bcd96771b137e1cce0a1d83f7c1be055787c6
senexia <128709556+senexia@users.noreply.github.com>
2025-07-28 21:17:39 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1744 ms, Memory: 301416 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] cnt = new int[10001];
+        for(int i = 0 ; i < n ; i++) {
+            cnt[Integer.parseInt(br.readLine())]++;
+        }
+
+        for(int i = 1 ; i < 10001 ; i++) {
+            for(int j = 0 ; j < cnt[i] ; j++) {
+                bw.write(String.valueOf(i) + "\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
