d75e98950c91282274e2f53f43ffb817e744a1d7
senexia <128709556+senexia@users.noreply.github.com>
2025-07-29 09:42:24 +0900

    [Silver III] Title: N과 M (6), Time: 100 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,46 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, r;
+    static int[] a, b;
+//    static boolean[] v;
+    static StringBuilder sb = new StringBuilder();
+    static void perm(int cnt, int st) {
+        if(cnt == r) {
+            for(int num : b) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i = st ; i < n ; i++) {
+//            if(v[i]) continue;
+//            v[i] = true;
+            b[cnt] = a[i];
+            perm(cnt + 1, i + 1);
+//            v[i] = false;
+        }
+
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        a = new int[n];
+        b = new int[r];
+//        v = new boolean[n];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < n ; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(a);
+        perm(0, 0);
+        sb.setLength(sb.length() - 1);
+        System.out.print(sb);
+    }
+}
