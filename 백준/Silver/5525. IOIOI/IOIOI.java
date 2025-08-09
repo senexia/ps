f4c894c41b92df3dfb27f92e9db8a53bff1de407
senexia <128709556+senexia@users.noreply.github.com>
2025-08-09 18:47:50 +0900

    [Silver I] Title: IOIOI, Time: 256 ms, Memory: 22176 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -4,22 +4,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine()); br.readLine();
         String s = br.readLine();
         String target = "IO".repeat(n) + "I";
         int cnt = 0;
+        int block = 0;
 
-        for(int i = 0 ; i <= s.length() - target.length() ; i++) {
-            boolean b = true;
-            if(s.charAt(i) == 'I') {
-                for(int j = 1 ; j < target.length() ; j++) {
-                    if(s.charAt(i + j) != target.charAt(j)) {
-                        b = false;
-                        break;
-                    }
+        for(int i = 1 ; i < s.length() - 1 ; i++) {
+            if(s.charAt(i) == 'O' && s.charAt(i - 1) == 'I' && s.charAt(i + 1) == 'I') {
+                block++;
+                if(block >= n) {
+                    cnt++;
+                    block--;
                 }
-                if(b) cnt++;
+                i = i + 1;
+            } else {
+                block = 0;
             }
         }
         System.out.println(cnt);
