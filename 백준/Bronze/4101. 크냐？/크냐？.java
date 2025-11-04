2951af9e1d9bc229fbd0a1b8e4037a933b1783d6
senexia <128709556+senexia@users.noreply.github.com>
2025-11-04 23:50:15 +0900

    [Bronze V] Title: 크냐?, Time: 108 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a == 0 && b == 0) break;
+            sb.append(a > b ? "Yes\n" : "No\n");
+        }
+        sb.setLength(sb.length() - 1);
+        System.out.print(sb);
+    }
+}
